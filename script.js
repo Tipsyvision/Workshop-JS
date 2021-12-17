3591039bbbf506ad253e644caf1fdf52ac505fb1
Onur Taskin <tipsyvision@hotmail.com>
2021-12-17 10:37:59 +0100

    10:37

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+
 //Scene
 console.log("hello Three.js");
-const scene = new THREE.Scene ()
+const scene = new THREE.Scene();
 
 //red cube
-const geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//sizes
+const sizes = {
+    width:800,
+    height:600
+}
+
+//camera
+const camera = new THREE.perpectivecamera(75, sizes.width / sizes.height)
+scene.add(camera)
+
+// renderer
+const renderer = new THREE.webGLRenderer ({
+console.log(canvas)
+    const canvas = document.querySelector('.webgl')   
+canvas: canvas
+
+})
